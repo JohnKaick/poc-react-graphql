791291e4346cff4fc11a1e975108fa0684bc9788
JohnKaick <john.kaick@gmail.com>
2019-08-26 07:46:18 -0300

    feature:

--- src/views/panel/Panel.jsx
+++ src/views/panel/Panel.jsx
@@ -34,7 +34,7 @@ export default compose(
         search: {
             mutation: M_SEARCH,
             refetchQueries: (mutationResult) => [
-                { query: Q_DELIVERY, args: {} }
+                { query: Q_DELIVERY, args: mutationResult.data.createDelivery }
             ]
         },
 
